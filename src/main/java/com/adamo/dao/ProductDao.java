d7b750bcb2340d1b1041f286a88981e18aa43b3b
Adamo08 <omarelaaddam519@gmail.com>
2024-12-12 18:26:06 +0100

    Refactor ProductDao to remove unused ```getProductsBy()``` Order method and clean up imports

--- src/main/java/com/adamo/dao/ProductDao.java
+++ src/main/java/com/adamo/dao/ProductDao.java
@@ -3,9 +3,11 @@ package com.adamo.dao;
 import com.adamo.model.Item;
 import com.adamo.util.DatabaseUtil;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class ProductDao {
 
@@ -19,38 +21,14 @@ public class ProductDao {
                 return new Item(
                         rs.getString("product_id"),
                         rs.getString("product_name"),
-                        0,  // Placeholder, will be updated later
+                        0,
                         rs.getDouble("price"),
-                        0   // Placeholder, will be updated later
+                        0
                 );
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
-    }
-
-    public List<Item> getProductsByOrder(List<String> productIds) {
-        String query = "SELECT * FROM products WHERE product_id IN (" + String.join(",", productIds) + ")";
-        List<Item> items = new ArrayList<>();
-        try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-            for (int i = 0; i < productIds.size(); i++) {
-                statement.setString(i + 1, productIds.get(i));
-            }
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                items.add(new Item(
-                        rs.getString("product_id"),
-                        rs.getString("product_name"),
-                        0,  // Placeholder, will be updated later
-                        rs.getDouble("price"),
-                        0   // Placeholder, will be updated later
-                ));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return items;
     }
 }
